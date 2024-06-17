8a6a0dab1723661772dd8111cd5a39cf76b14c34
Karriem Hoe <karriem72402@gmail.com>
2024-06-17 19:05:09 -0400

    third commit

--- Main/cells.py
+++ Main/cells.py
@@ -13,13 +13,13 @@ class Cell:
             text='text'
         )
         btn.bind('<Button-1>', self.left_click_actions ) # Left Click
-        btn.bind('<Button-3>', self.left_click_actions ) # Right Click
+        btn.bind('<Button-3>', self.right_click_actions ) # Right Click
         self.cell_btn_object = btn
 
     def left_click_actions(self, event):
         print(event)
         print('Left clicked!')
 
-    def left_click_actions(self, event):
+    def right_click_actions(self, event):
         print(event)
         print('Right clicked!')
